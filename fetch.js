3f0ae19469066c64eeb9a473a7c9f663668c1a65
Nish7 <nishilkapadia17@gmail.com>
2022-12-03 12:04:56 -0500

    comments

--- fetch.js
+++ fetch.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
 
 (async () => {
+	// Read files from songs.txt
 	const file = fs.readFileSync('songs.txt', 'utf-8');
+	// each line seperated using regex
 	const song = file.split(/\r?\n/);
 
+	// return data object
 	const d = [];
 
+	// fetch songs 50 at a time -> to avoid rate limits
 	for (let i = 0; i < song.length; i += 50) {
 		const res = await fetchSongs(song.slice(i, i + 50));
 		d.push(...res);
 		console.log(`Round ${i}`);
+
+		// wait for 30sec.
 		await sleep(30000);
 	}
 
-	console.log(d);
-
+	// write the response into a new file.
 	fs.writeFile(
 		'songs_meta_fetch.json',
 		JSON.stringify(d),
@@ -27,9 +32,11 @@ const fs = require('fs');
 })();
 
 async function fetchSongs(song) {
+	// this is  expired
 	const oauth =
 		'BQD_jOuTQZnUq9GOqrSNrhT223p1tfRMP-2eaCOcQ-rf6Sn9xgfcQwmrWaf_i6Gg9BaONODMkPkzVD6BFWmIRGptYocO6IQ1ZSMKyeKI9rY33iCR8iI6wHrW-Sn9ifgC-LX-MfRQ2MSIn-O53Fej4ugHN0bmzEboAcPxp4cpnLVSNYyv27jFWN6uM5trG4BhhGk';
 
+	// 50 parallel request
 	const promises = song.map((line, i) => {
 		const url = new URL('https://api.spotify.com/v1/search');
 		url.searchParams.set('q', line);
@@ -45,8 +52,10 @@ async function fetchSongs(song) {
 			.catch((e) => console.log(e));
 	});
 
+	// wait for all of the promises to be fullfilled
 	let a = await Promise.all(promises);
 
+	// data clean up
 	const rf_a = a.map(({ tracks }) => ({
 		href: tracks.href,
 		name: tracks?.items[0]?.name,
--- import.js
+++ import.js
@@ -1,6 +1,7 @@
 const songs = require('./songs_meta_fetch');
 
 (async () => {
+	// import 100 tracks at a time: rate limit
 	for (let i = 0; i < songs.length; i += 100) {
 		await importSongs(songs.slice(i, i + 100));
 		console.log(`Round ${i} Completed!`);
@@ -9,14 +10,15 @@ const songs = require('./songs_meta_fetch');
 })();
 
 async function importSongs(hund_songs) {
+	// this is expired.
 	const oauth =
 		'BQDcs8LQUXH8OZcuUXxuTxZkYYa0kRsljrJfUK9HLHyfML9ajhHgyJQltrfW18BH1NSpD292M4VsMQYtbogUByPKmFWWqWy4uFDvOg5nf750SqXLtgmq01s0ot8_IW_IRSyytERqbEPrPEl9UwjPE-XcYzmxRnUxZz-3BsIuCqUrTwNyBb4hz-0Z0NQbMc7eFoeK9RR73OSDmH2A972xLe-ahMdP8M9a1Lcz04py5W-CU6XoMqayzpFfXpKJgUew';
 	const playlist_id = '01OwSzdAeUP5A4L8YCGQ9k';
 
+	// passed the ids into the body of the request
 	let ids = hund_songs.map((s) => s.id);
+	// set used to remove duplicates
 	ids = [...new Set(ids)].map((id) => 'spotify:track:' + id);
-
-	console.log(ids);
 
 	const d = await fetch(
 		`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`,
